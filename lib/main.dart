f4d255b4b6094185a257ce7dc8cd3bf855db6a60
Rohith Paramasivam <rohithparamasivam1125@gmail.com>
2026-07-22 13:04:30 +0530

    Build 3 release upload for user in-app update test

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ import 'services/database_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-const int kCurrentVersionCode = 2;
-const String kCurrentVersionName = '1.0.2';
+const int kCurrentVersionCode = 3;
+const String kCurrentVersionName = '1.0.3';
 
 // Developer update Build 2 (Features & Bugfixes)
 void main() async {
